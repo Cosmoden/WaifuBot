93c323aa3b6351cda95fb27ab4a3c96807ecb181
Cosmoden <dennis.lavrushin@gmail.com>
2023-02-09 10:07:14 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,69 @@
+import threading
+
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler, ConversationHandler, CallbackQueryHandler
+import logging
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
+updater = None
+
+
+def start(update, context):
+    update.message.reply_text("Привет!")
+    context.user_data['messages'] = []
+
+
+def help(update, context):
+    update.message.reply_text('''Этот бот может присылать случайные арты в разных категориях \n\n
+    Доступные ''')
+
+
+def write_message(update, context):
+    context.user_data['messages'].append(update.message.text)
+
+
+def error(update, context):
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def music(update, context):
+    query = ' '.join(map(str, context.user_data['messages']))
+    context.user_data['messages'] = []
+    text = "Вот ваши рекомендации: / " \
+           "Here are your recommendations: \n"
+    res = fast_search.get_songs(query)
+    for song in res:
+        text += f'{song[1]} - {song[0]} \n'
+    update.message.reply_text(text)
+
+
+def stop(update, context):
+    update.message.reply_text("Приятного прослушивания!")
+    threading.Thread(target=shutdown).start()
+
+
+def main():
+    global updater
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("music", music))
+    dp.add_handler(CommandHandler("stop", stop))
+    dp.add_handler(MessageHandler(Filters.text, write_message))
+    dp.add_error_handler(error)
+    updater.start_polling()
+    updater.idle()
+
+
+def shutdown():
+    updater.stop()
+    updater.is_idle = False
+
+
+if __name__ == '__main__':
+    main()
